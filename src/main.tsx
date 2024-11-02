cac4e8f0167a33d1b74f76dbb582f0ebe325e9ac
eladjmc <eladjmc88@gmail.com>
2024-11-02 15:47:17 +0200

    added the context and all the comomponents - application live

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { CounterProvider } from "./context/CounterContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <CounterProvider>
     <App />
-  </StrictMode>,
-)
+  </CounterProvider>
+);
--- src/pages/CounterPage/CounterPage.tsx
+++ src/pages/CounterPage/CounterPage.tsx
@@ -1,9 +1,12 @@
 import { FC } from "react";
 import "./CounterPage.css"
+import CounterDisplay from "../../components/CounterDisplay/CounterDisplay";
+import CounterActions from "../../components/CounterActions/CounterActions";
 
 const CounterPage: FC = () => {
   return <main className="page CounterPage">
-    counter page
+    <CounterDisplay/>
+    <CounterActions/>
   </main>;
 };
 
