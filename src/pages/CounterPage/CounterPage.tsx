8818bc2163b81fad9fe918cf65ce3c38b06305ae
eladjmc <eladjmc88@gmail.com>
2024-11-02 14:13:57 +0200

    cleaned project and added the counter page tamplate

---
+++ src/pages/CounterPage/CounterPage.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import "./CounterPage.css"
+
+const CounterPage: FC = () => {
+  return <main className="page CounterPage">
+    counter page
+  </main>;
+};
+
+export default CounterPage;
